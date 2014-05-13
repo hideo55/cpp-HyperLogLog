52d370049aabd78cd3fd088c3b6c096e71bf2e41
hideo55 <hide.o.j55@gmail.com>
2014-05-14 07:59:29 +0900

    Update tests

--- t/HyperLogLogTest.cpp
+++ t/HyperLogLogTest.cpp
@@ -7,54 +7,49 @@
 using namespace igloo;
 using namespace hll;
 
-
-static const char alphanum[] =
-"0123456789"
-"!@#$%^&*"
-"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-"abcdefghijklmnopqrstuvwxyz";
+static const char alphanum[] = "0123456789"
+        "!@#$%^&*"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
 
 static const int alphanumSize = sizeof(alphanum) - 1;
 
 static void genRandomString(size_t len, std::string& str) {
     srand(time(0));
-    for(size_t i = 0; i < len; ++i){
+    for (size_t i = 0; i < len; ++i) {
         char c = alphanum[rand() % alphanumSize];
         str.append(&c, 1);
     }
 }
 
-Describe(normal_pattern) 
-{
-    It(create_instance) 
-    {
+Describe(hll_HyperLogLog) {
+    It(create_instance) {
         HyperLogLog *hll = new HyperLogLog(10);
         Assert::That(hll != NULL);
         delete hll;
     }
 
-    It(estimate)
-    {
-        HyperLogLog *hll = new HyperLogLog(16);
-        std::map <std::string, bool> unique;
-        size_t dataNum = 1000;
-        for(size_t i = 1; i < dataNum; ++i){
-            std::string str;
-            do {
-                genRandomString(10, str);
-            } while(unique.find(str) != unique.end());
-            unique.insert(std::make_pair(str, true));
-            hll->add(str.c_str(), str.size());
+    It(estimate) {
+        for (int n = 0; n < 10; ++n) {
+            HyperLogLog *hll = new HyperLogLog(16);
+            std::map<std::string, bool> unique;
+            size_t dataNum = 1000;
+            for (int i = 1; i < dataNum; ++i) {
+                std::string str;
+                do {
+                    genRandomString(10, str);
+                } while (unique.find(str) != unique.end());
+                unique.insert(std::make_pair(str, true));
+                hll->add(str.c_str(), str.size());
+            }
+            double cardinality = hll->estimate();
+            double errorRatio = abs(dataNum - cardinality) / dataNum;
+            Assert::That(errorRatio, IsLessThan(0.01));
         }
-        double cardinality = hll->estimate();
-        double errorRatio = abs(dataNum - cardinality)/dataNum;
-        Assert::That(errorRatio < 0.01);
     }
 };
 
-int
-main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
     return TestRunner::RunAllTests(argc, argv);
 }
 
