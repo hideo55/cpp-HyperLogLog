e104d7460810cd5f0d369c61c11dd0e818539e1f
hideo55 <hide.o.j55@gmail.com>
2014-05-15 07:39:51 +0900

    Updated docs

--- search/functions_65.js
+++ search/functions_65.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['estimate',['estimate',['../classhll_1_1_hyper_log_log.html#ac4fb03ba2d455613d4487dafd1d80177',1,'hll::HyperLogLog']]]
+  ['estimate',['estimate',['../classhll_1_1_hyper_log_log.html#aaa9d7dd08722a22ecbf3e48ed6720358',1,'hll::HyperLogLog']]]
 ];
