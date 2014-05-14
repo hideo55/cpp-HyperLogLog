1ede858e6acfb1a363358f9afc130bd6aed690e4
hideo55 <hide.o.j55@gmail.com>
2014-05-15 03:20:33 +0900

    Updated docs

---
+++ search/all_63.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['clear',['clear',['../classhll_1_1_hyper_log_log.html#a66265cc0d8a73c2ab71daec10383dd45',1,'hll::HyperLogLog']]]
+];
--- search/functions_72.js
+++ search/functions_72.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['registersize',['registerSize',['../classhll_1_1_hyper_log_log.html#a1b832419b07fa9dcca1e993c4f6eb47f',1,'hll::HyperLogLog']]],
   ['rotl32',['rotl32',['../murmur3_8h.html#a1bb665f421e67f5adc7b8acd3cda136e',1,'murmur3.h']]]
 ];
