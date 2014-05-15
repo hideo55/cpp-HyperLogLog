af6d7ee7668be4e5dac36f8d11db8017b93c01f2
Hideaki Ohno <hide.o.j55@gmail.com>
2014-05-15 10:40:43 +0900

    Update tests [skip ci]

--- t/HyperLogLogTest.cpp
+++ t/HyperLogLogTest.cpp
@@ -1,5 +1,5 @@
 #include <igloo/igloo_alt.h>
-#include "hyperloglog.h"
+#include "hyperloglog.hpp"
 #include <map>
 #include <string>
 #include <cstdlib>
@@ -7,34 +7,49 @@
 using namespace igloo;
 using namespace hll;
 
-static const char alphanum[] = "0123456789"
-        "!@#$%^&*"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
 
-static const int alphanumSize = sizeof(alphanum) - 1;
+namespace {
+	// Test utilities
 
-static void genRandomString(size_t len, std::string& str) {
-    srand(time(0));
-    for (size_t i = 0; i < len; ++i) {
-        char c = alphanum[rand() % alphanumSize];
-        str.append(&c, 1);
-    }
-}
+	static const char alphanum[] = "0123456789"
+    	    "!@#$%^&*"
+        	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        	"abcdefghijklmnopqrstuvwxyz";
 
-static std::map<std::string, bool> GEN_STRINGS;
-static void getUniqueString(std::string& str) {
-    do {
-        genRandomString(10, str);
-    } while (GEN_STRINGS.find(str) != GEN_STRINGS.end());
-    GEN_STRINGS.insert(std::make_pair(str, true));
+	static const int alphanumSize = sizeof(alphanum) - 1;
+
+	static void genRandomString(size_t len, std::string& str) {
+    	srand(time(0));
+    	for (size_t i = 0; i < len; ++i) {
+        	char c = alphanum[rand() % alphanumSize];
+        	str.append(&c, 1);
+    	}
+	}
+
+	static std::map<std::string, bool> GEN_STRINGS;
+	static void getUniqueString(std::string& str) {
+    	do {
+        	genRandomString(10, str);
+    	} while (GEN_STRINGS.find(str) != GEN_STRINGS.end());
+    	GEN_STRINGS.insert(std::make_pair(str, true));
+	}
 }
 
 Describe(hll_HyperLogLog) {
     It(create_instance) {
-        HyperLogLog *hll = new HyperLogLog(10);
+        HyperLogLog *hll = new HyperLogLog(4);
         Assert::That(hll != NULL);
         delete hll;
+
+        hll = new HyperLogLog(16);
+        Assert::That(hll != NULL);
+        delete hll;
+
+        AssertThrows(std::invalid_argument, HyperLogLog(3));
+        Assert::That(LastException<std::invalid_argument>().what(), Is().Containing("bit width must be in the range [4,16]"));
+		
+        AssertThrows(std::invalid_argument, HyperLogLog(17));
+        Assert::That(LastException<std::invalid_argument>().what(), Is().Containing("bit width must be in the range [4,16]"));
     }
 
     It(register_size) {
@@ -64,28 +79,30 @@ Describe(hll_HyperLogLog) {
     }
 
     It(merge) {
-        HyperLogLog *hll = new HyperLogLog(16);
+        HyperLogLog hll(16);
         size_t dataNum = 100;
         for (int i = 1; i < dataNum; ++i) {
             std::string str;
             getUniqueString(str);
-            hll->add(str.c_str(), str.size());
+            hll.add(str.c_str(), str.size());
         }
 
-        HyperLogLog *hll2 = new HyperLogLog(16);
+        HyperLogLog hll2(16);
         size_t dataNum2 = 200;
         for (int i = 1; i < dataNum2; ++i) {
             std::string str;
             getUniqueString(str);
-            hll2->add(str.c_str(), str.size());
+            hll2.add(str.c_str(), str.size());
         }
 
-        hll->merge(*hll2);
-        double cardinality = hll->estimate();
-        delete hll;
-        delete hll2;
+        hll.merge(hll2);
+        double cardinality = hll.estimate();
         double errorRatio = abs(dataNum + dataNum2 - cardinality) / (dataNum + dataNum2);
         Assert::That(errorRatio, IsLessThan(0.01));
+
+        HyperLogLog hll3(10);
+        AssertThrows(std::invalid_argument, hll.merge(hll3));
+        Assert::That(LastException<std::invalid_argument>().what(), Is().Containing("number of registers doesn't match:"));
     }
 
     It(clear){
