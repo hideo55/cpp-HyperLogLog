586de02ce0dc904311d0a88364ac1c1ceaf4c1f5
hideo55 <hide.o.j55@gmail.com>
2014-05-15 23:55:35 +0900

    Updated docs

--- search/all_68.js
+++ search/all_68.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['hll',['hll',['../namespacehll.html',1,'']]],
-  ['hll_5fhash_5fseed',['HLL_HASH_SEED',['../hyperloglog_8h.html#a21231cbc051c73769ed18fc81ef83f49',1,'hyperloglog.h']]],
+  ['hll_5fhash_5fseed',['HLL_HASH_SEED',['../hyperloglog_8hpp.html#a21231cbc051c73769ed18fc81ef83f49',1,'hyperloglog.hpp']]],
   ['hyperloglog',['HyperLogLog',['../classhll_1_1_hyper_log_log.html#a29392bbe10af3675869d76a0df06a370',1,'hll::HyperLogLog']]],
   ['hyperloglog',['HyperLogLog',['../classhll_1_1_hyper_log_log.html',1,'hll']]],
-  ['hyperloglog_2eh',['hyperloglog.h',['../hyperloglog_8h.html',1,'']]]
+  ['hyperloglog_2ehpp',['hyperloglog.hpp',['../hyperloglog_8hpp.html',1,'']]]
 ];
